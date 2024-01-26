ebc6799dd9aca41e516a53cef67596a04fb864fd
austin1610 <austinsparks09@gmail.com>
2024-01-26 00:21:45 -0600

    Added new method that makes checkmark visible if corresponding "row checked" is true or hides it if false

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -83,7 +83,31 @@ class ChecklistViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+    func configureCheckmark(
+       for cell: UITableViewCell,
+       at indexPath: IndexPath
+    ) {
+       var isChecked = false
+        
+       if indexPath.row == 0 {
+       isChecked = row0checked
+       } else if indexPath.row == 1 {
+         isChecked = row1checked
+       } else if indexPath.row == 2 {
+         isChecked = row2checked
+       } else if indexPath.row == 3 {
+         isChecked = row3checked
+       } else if indexPath.row == 4 {
+         isChecked = row4checked
+       }
+        
+       if isChecked {
+         cell.accessoryType = .checkmark
+       } else {
+         cell.accessoryType = .none
+       }
+    }
 
 }
 
