d52b40a8248a896766e9e9e5f773cda92ae1ec6d
austin1610 <austinsparks09@gmail.com>
2024-01-26 16:02:26 -0600

    Replaced my code with proper code from source code because I had a problem and couldnt find it

--- Checklists/AddItemTableViewController.swift
+++ Checklists/AddItemTableViewController.swift
@@ -7,72 +7,56 @@
 
 import UIKit
 
-protocol AddItemViewControllerDelegate: AnyObject {
-   func addItemViewControllerDidCancel(
-     _ controller: AddItemViewController)
-   func addItemViewController(
-     _ controller: AddItemViewController,
-     didFinishAdding item: ChecklistItem
- )
+protocol AddItemViewControllerDelegate: class {
+  func addItemViewControllerDidCancel(_ controller: AddItemViewController)
+  func addItemViewController(
+    _ controller: AddItemViewController,
+    didFinishAdding item: ChecklistItem
+  )
 }
 
-class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
-    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
-    
-    weak var delegate: AddItemViewControllerDelegate?
-    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+  weak var delegate: AddItemViewControllerDelegate?
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        navigationItem.largeTitleDisplayMode = .never
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        textField.becomeFirstResponder()
-    }
-    
-    // MARK: - Actions
-    @IBAction func cancel() {
-        delegate?.addItemViewControllerDidCancel(self)
-    }
-    
-    @IBAction func done() {
-       let item = ChecklistItem()
-       item.text = textField.text!
-        
-       delegate?.addItemViewController(self, didFinishAdding: item)
-    }
-    
-    // MARK: - Table View Delegates
-    override func tableView(
-      _ tableView: UITableView,
-      willSelectRowAt indexPath: IndexPath
-    ) -> IndexPath? {
-      return nil
-    }
-    // MARK: - Text Field Delegates
-    func textField(
-      _ textField: UITextField,
-      shouldChangeCharactersIn range: NSRange,
-      replacementString string: String
-    ) -> Bool {
-      let oldText = textField.text!
-      let stringRange = Range(range, in: oldText)!
-      let newText = oldText.replacingCharacters(
-      in: stringRange,
-      with: string)
-      if newText.isEmpty {
-      doneBarButton.isEnabled = false
-     } else {
-      doneBarButton.isEnabled = true
-     }
-      return true
-    }
-    
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        doneBarButton.isEnabled = false
-        return true
-    }
+    super.viewDidLoad()
+    navigationItem.largeTitleDisplayMode = .never
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
+
+  // MARK: - Actions
+  @IBAction func cancel() {
+    delegate?.addItemViewControllerDidCancel(self)
+  }
+
+  @IBAction func done() {
+    let item = ChecklistItem()
+    item.text = textField.text!
+    delegate?.addItemViewController(self, didFinishAdding: item)
+  }
+
+  // MARK: - Table View Delegates
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    return nil
+  }
+
+  // MARK: - Text Field Delegates
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    let oldText = textField.text!
+    let stringRange = Range(range, in: oldText)!
+    let newText = oldText.replacingCharacters(in: stringRange, with: string)
+    doneBarButton.isEnabled = !newText.isEmpty
+    return true
+  }
+
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    doneBarButton.isEnabled = false
+    return true
+  }
 }
