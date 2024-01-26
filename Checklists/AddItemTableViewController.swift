846438fda521821c1e00ef6672433670225aaa0e
austin1610 <austinsparks09@gmail.com>
2024-01-26 13:40:30 -0600

    Connected textfield to AddItemTableViewController

--- Checklists/AddItemTableViewController.swift
+++ Checklists/AddItemTableViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class AddItemTableViewController: UITableViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
