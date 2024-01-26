eb67bbe4fc476f2f04c53ce0846fb7a396226445
austin1610 <austinsparks09@gmail.com>
2024-01-26 10:31:30 -0600

    Simplified code and allowed each method to support the array and in numberOfRowsInSection() now only returns the actual numbers of items

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ChecklistViewController: UITableViewController {
     
     var items = [ChecklistItem]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,7 +42,7 @@ class ChecklistViewController: UITableViewController {
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     )   -> Int {
-        return 100
+        return items.count
     }
     
     override func tableView(
@@ -53,19 +53,10 @@ class ChecklistViewController: UITableViewController {
             withIdentifier: "ChecklistItem",
             for: indexPath)
         
-        let label = cell.viewWithTag(1000) as! UILabel
-        if indexPath.row == 0 {
-         label.text = row0item.text
-        } else if indexPath.row == 1 {
-         label.text = row1item.text
-        } else if indexPath.row == 2 {
-         label.text = row2item.text
-        } else if indexPath.row == 3 {
-         label.text = row3item.text
-        } else if indexPath.row == 4 {
-         label.text = row4item.text
-        }
+        let item = items[indexPath.row]
         
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
         configureCheckmark(for: cell, at: indexPath)
         return cell
     }
@@ -76,45 +67,24 @@ class ChecklistViewController: UITableViewController {
         didSelectRowAt indexPath: IndexPath
     )   {
         if let cell = tableView.cellForRow(at: indexPath) {
-            if indexPath.row == 0 {
-             row0item.checked.toggle()
-            } else if indexPath.row == 1 {
-             row1item.checked.toggle()
-            } else if indexPath.row == 2 {
-             row2item.checked.toggle()
-            } else if indexPath.row == 3 {
-             row3item.checked.toggle()
-            } else if indexPath.row == 4 {
-             row4item.checked.toggle()
-            }
-          configureCheckmark(for: cell, at: indexPath)
+            let item = items[indexPath.row]
+            item.checked.toggle()
+            
+            configureCheckmark(for: cell, at: indexPath)
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     func configureCheckmark(
-       for cell: UITableViewCell,
-       at indexPath: IndexPath
+        for cell: UITableViewCell,
+        at indexPath: IndexPath
     ) {
-       var isChecked = false
+        let item = items[indexPath.row]
         
-        if indexPath.row == 0 {
-         isChecked = row0item.checked
-        } else if indexPath.row == 1 {
-         isChecked = row1item.checked
-        } else if indexPath.row == 2 {
-         isChecked = row2item.checked
-        } else if indexPath.row == 3 {
-         isChecked = row3item.checked
-        } else if indexPath.row == 4 {
-         isChecked = row4item.checked
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
         }
-        
-       if isChecked {
-         cell.accessoryType = .checkmark
-       } else {
-         cell.accessoryType = .none
-       }
     }
 }
-
