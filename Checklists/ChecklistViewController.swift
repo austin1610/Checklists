3247b5da1a1bdbf029c8e578834be30a5ec06df2
austin1610 <austinsparks09@gmail.com>
2024-01-26 20:38:01 -0600

    Changed code to switch checkmark on and off

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -38,9 +38,12 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
 
   func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
     if item.checked {
-      cell.accessoryType = .checkmark
-    } else {
-      cell.accessoryType = .none
+      let label = cell.viewWithTag(1001) as! UILabel
+         
+      if item.checked {
+        label.text = "√"
+      } else {
+        label.text = ""
     }
   }
 
