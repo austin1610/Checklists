732268232f8c3fa6746fded7a55fa67912f1f55b
austin1610 <austinsparks09@gmail.com>
2024-01-30 22:59:08 -0600

    sends checklist object from the row the user taps on

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -47,7 +47,8 @@ class AllListsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "ShowChecklist", sender: nil)
+        let checklist = lists[indexPath.row]
+        performSegue(withIdentifier: "ShowChecklist", sender: checklist)
     }
 
 }
