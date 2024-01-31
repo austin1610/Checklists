53ade3d33ffc7fc8da141344d4c3d4dfea4f78b2
austin1610 <austinsparks09@gmail.com>
2024-01-30 23:31:39 -0600

    Added the action methods for done and cancel buttons

--- Checklists/ListDetailViewController.swift
+++ Checklists/ListDetailViewController.swift
@@ -39,4 +39,24 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate {
         textField.becomeFirstResponder()
     }
     
+    // MARK: - Actions
+    @IBAction func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    @IBAction func done() {
+        if let checklist = checklistToEdit {
+            checklist.name = textField.text!
+            delegate?.listDetailViewController(
+                self,
+                didFinishEditing: checklist)
+        } else {
+            let checklist = Checklist(name: textField.text!)
+            delegate?.listDetailViewController(
+                self,
+                didFinishAdding: checklist)
+        }
+        
+    }
+    
 }
