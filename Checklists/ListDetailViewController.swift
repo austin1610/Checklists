e67bfdb8d9d79b6cb6ac789aa77a54fb2763c8d4
austin1610 <austinsparks09@gmail.com>
2024-01-30 23:29:06 -0600

    Added code to the ListDetailVC

--- Checklists/ListDetailViewController.swift
+++ Checklists/ListDetailViewController.swift
@@ -5,4 +5,38 @@
 //  Created by Austin Sparks on 1/30/24.
 //
 
-import Foundation
+import UIKit
+
+protocol ListDetailViewControllerDelegate: AnyObject {
+    func listDetailViewControllerDidCancel(_ controller: ListDetailViewController)
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishAdding checklist: Checklist)
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist)
+    
+}
+
+class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    
+    var checklistToEdit: Checklist?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = checklist.name
+            doneBarButton.isEnabled = true
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+}
