da8d40e7e8a0c121e72dc39e3194676f4ccfa50c
austin1610 <austinsparks09@gmail.com>
2024-01-30 20:20:06 -0600

    registers our cell identifier with the table view so that the table view knows which cell class should be used to create a new table view cell instance when a dequeue request comes in with that cell identifier

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -14,6 +14,7 @@ class AllListsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
     }
 
     // MARK: - Table view data source
