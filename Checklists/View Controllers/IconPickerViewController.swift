4ac4e1a9c7c7dfd76169e1770de736b0bbd3813d
austin1610 <austinsparks09@gmail.com>
2024-02-01 10:56:27 -0600

    Fixed where my curly bracket are

--- Checklists/View Controllers/IconPickerViewController.swift
+++ Checklists/View Controllers/IconPickerViewController.swift
@@ -15,22 +15,22 @@ protocol IconPickerViewControllerDelegate: AnyObject {
 class IconPickerViewController: UITableViewController {
     weak var delegate: IconPickerViewControllerDelegate?
     let icons = [
-     "No Icon", "Appointments", "Birthdays", "Chores",
-     "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips"
+        "No Icon", "Appointments", "Birthdays", "Chores",
+        "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips"
     ]
-}
-
-// MARK: - Table View Delegates
-override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return icons.count
     
-}
-
-override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
-    let iconName = icons[indexPath.row]
-    cell.textLabel!.text = iconName
-    cell.imageView!.image = UIImage(named: iconName)
-    return cell
+    // MARK: - Table View Delegates
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+        
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
+        let iconName = icons[indexPath.row]
+        cell.textLabel!.text = iconName
+        cell.imageView!.image = UIImage(named: iconName)
+        return cell
+        
+    }
 }
