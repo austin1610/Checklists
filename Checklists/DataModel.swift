9d06ec0436911774320a0f63fcb3aa905dc3efb9
austin1610 <austinsparks09@gmail.com>
2024-01-31 21:54:07 -0600

    Code for when app runs for first time a new checklist object is added to array

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -62,5 +62,18 @@ class DataModel {
         [String: Any]
         UserDefaults.standard.register(defaults: dictionary)
     }
+    
+    func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+        }
+    }
 
 }
