838ad1a79d6f1b75b16741c91388d42dbd7b69f9
austin1610 <austinsparks09@gmail.com>
2024-02-01 01:21:59 -0600

    Changed content of new VC

--- Checklists/View Controllers/IconPickerViewController.swift
+++ Checklists/View Controllers/IconPickerViewController.swift
@@ -5,4 +5,14 @@
 //  Created by Austin Sparks on 2/1/24.
 //
 
-import Foundation
+import UIKit
+
+protocol IconPickerViewControllerDelegate: AnyObject {
+    func iconPicker(_ picker: IconPickerViewController, didPick iconName: String)
+    
+}
+
+class IconPickerViewController: UITableViewController {
+    weak var delegate: IconPickerViewControllerDelegate?
+    
+}
