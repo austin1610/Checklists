573d6d2a142616630d879d2fcec1e419af0ff148
austin1610 <austinsparks09@gmail.com>
2024-01-31 22:21:48 -0600

    Moved code from datamodel to checklist swift... oops added it to wrong place

--- Checklists/Data Model/DataModel.swift
+++ Checklists/Data Model/DataModel.swift
@@ -76,13 +76,5 @@ class DataModel {
             userDefaults.set(false, forKey: "FirstTime")
         }
     }
-    
-    func countUncheckedItems() -> Int {
-        var count = 0
-        for item in items where !item.checked {
-            count += 1
-        }
-        return count
-    }
 
 }
