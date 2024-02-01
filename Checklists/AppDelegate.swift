93aaef45bd448ba53daaded63f5c7a80f60e783e
austin1610 <austinsparks09@gmail.com>
2024-01-31 18:00:58 -0600

    Added unneeded code from ChecklistVC to appdelegate and commented it out for later use (it will not be used in this file just being stored here)

--- Checklists/AppDelegate.swift
+++ Checklists/AppDelegate.swift
@@ -34,3 +34,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+// Methods needed later
+
+//func documentsDirectory() -> URL {
+//   let paths = FileManager.default.urls(
+//     for: .documentDirectory,
+//     in: .userDomainMask)
+//   return paths[0]
+//}
+
+//func dataFilePath() -> URL {
+//   return
+//  documentsDirectory().appendingPathComponent("Checklists.plist")
+//}
+
+//func saveCheckListItems() {
+//    let encoder = PropertyListEncoder()
+//    do {
+//        let data = try encoder.encode(checklist.items)
+//        try data.write(
+//          to: dataFilePath(),
+//          options: Data.WritingOptions.atomic)
+//    } catch {
+//        print("Error encoding item array: \(error.localizedDescription)")
+//    }
+//}
+
+//func loadCheckListItems() {
+//    let path = dataFilePath()
+//    if let data = try? Data(contentsOf: path) {
+//        let decoder = PropertyListDecoder()
+//        do {
+//            checklist.items = try decoder.decode(
+//              [ChecklistItem].self,
+//              from: data)
+//        } catch {
+//            print("Error decoding item array: \(error.localizedDescription)")
+//        }
+//    }
+//      
+//}
+
