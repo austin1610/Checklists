4bbd76f0bd09ed354d8dc275c3c37c5433418a6d
austin1610 <austinsparks09@gmail.com>
2024-02-01 01:28:03 -0600

    added methods that will obtain a table view cell and give it a title and an image

--- Checklists/View Controllers/IconPickerViewController.swift
+++ Checklists/View Controllers/IconPickerViewController.swift
@@ -19,3 +19,18 @@ class IconPickerViewController: UITableViewController {
      "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips"
     ]
 }
+
+// MARK: - Table View Delegates
+override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return icons.count
+    
+}
+
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
+    let iconName = icons[indexPath.row]
+    cell.textLabel!.text = iconName
+    cell.imageView!.image = UIImage(named: iconName)
+    return cell
+    
+}
