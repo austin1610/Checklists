83ff5842b2070c28954e7a7147bf529f700830ea
austin1610 <austinsparks09@gmail.com>
2024-02-01 00:55:51 -0600

    Removed code because reloadData() is done on the table view

--- Checklists/View Controllers/AllListsViewController.swift
+++ Checklists/View Controllers/AllListsViewController.swift
@@ -109,24 +109,17 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
   }
 
   func listDetailViewController(_ controller: ListDetailViewController, didFinishAdding checklist: Checklist) {
-    let newRowIndex = dataModel.lists.count
-    dataModel.lists.append(checklist)
-
-    let indexPath = IndexPath(row: newRowIndex, section: 0)
-    let indexPaths = [indexPath]
-    tableView.insertRows(at: indexPaths, with: .automatic)
-
-    navigationController?.popViewController(animated: true)
+      dataModel.lists.append(checklist)
+      dataModel.sortChecklists() 
+      tableView.reloadData()
+      navigationController?.popViewController(animated: true)
   }
 
   func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist) {
-    if let index = dataModel.lists.firstIndex(of: checklist) {
-      let indexPath = IndexPath(row: index, section: 0)
-      if let cell = tableView.cellForRow(at: indexPath) {
-        cell.textLabel!.text = checklist.name
-      }
-    }
-    navigationController?.popViewController(animated: true)
+      dataModel.sortChecklists()
+      tableView.reloadData()
+      navigationController?.popViewController(animated: true)
+      
   }
 
 }
