ff5cc4577b2652169ffc6445586935710f88b072
austin1610 <austinsparks09@gmail.com>
2024-01-31 20:42:53 -0600

    Removed data saving from all list from controller and added it to datamodel.swift

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -94,39 +94,6 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     }
     navigationController?.popViewController(animated: true)
   }
-    
-  // MARK: - Data Saving
-  func documentsDirectory() -> URL {
-      let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-      return paths[0]
-  }
-    
-  func dataFilePath() -> URL {
-    return documentsDirectory().appendingPathComponent("Checklists.plist")
-  }
-    
-  func saveChecklists() {
-      let encoder = PropertyListEncoder()
-      do {
-        let data = try encoder.encode(lists)
-        try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
-      } catch {
-            print("Error encoding item array: \(error.localizedDescription)")
-      }
-  }
-    
-  func loadCheckList() {
-      let path = dataFilePath()
-      if let data = try? Data(contentsOf: path) {
-          let decoder = PropertyListDecoder()
-          do {
-              lists = try decoder.decode(
-                [Checklist].self, from: data)
-          } catch {
-              print("Error decoding list array: \(error.localizedDescription)")
-          }
-      }
-  }
 
 }
 
