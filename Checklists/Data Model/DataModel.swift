18a12576b7f8b47924130de0eecf128b90dc8775
austin1610 <austinsparks09@gmail.com>
2024-01-31 22:11:56 -0600

    counts how many unchecked items there are in a list

--- Checklists/Data Model/DataModel.swift
+++ Checklists/Data Model/DataModel.swift
@@ -76,5 +76,13 @@ class DataModel {
             userDefaults.set(false, forKey: "FirstTime")
         }
     }
+    
+    func countUncheckedItems() -> Int {
+        var count = 0
+        for item in items where !item.checked {
+            count += 1
+        }
+        return count
+    }
 
 }
