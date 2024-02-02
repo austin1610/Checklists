6c2a17cd0e897942cecdfb306357ff4eb7886bac
austin1610 <austinsparks09@gmail.com>
2024-02-01 23:05:38 -0600

    This method gets the current “ChecklistItemID” value from UserDefaults, adds 1 to it, and writes it back to UserDefaults. It returns the previous value to the caller

--- Checklists/Data Model/DataModel.swift
+++ Checklists/Data Model/DataModel.swift
@@ -83,5 +83,12 @@ class DataModel {
             userDefaults.set(false, forKey: "FirstTime")
         }
     }
+    
+    class func nextChecklistItemID() -> Int {
+        let userDefaults = UserDefaults.standard
+        let itemID = userDefaults.integer(forKey: "ChecklistItemID")
+        userDefaults.set(itemID + 1, forKey: "ChecklistItemID")
+        return itemID
+    }
 
 }
